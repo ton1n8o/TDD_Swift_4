85101476af2f1586a40fc21b4a1e04336c1c0f9a
Antônio Carlos <antoniocarlos.dev@gmail.com>
2017-07-21 12:19:54 +0800

    Project evolution till page 127

---
+++ ToDo/Controller/InputViewController.swift
@@ -0,0 +1,69 @@
+//
+//  InputViewController.swift
+//  ToDo
+//
+//  Created by Antonio da Silva on 21/07/2017.
+//  Copyright © 2017 TNTStudios. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class InputViewController: UIViewController {
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var dateTextField: UITextField!
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var addressTextField: UITextField!
+    @IBOutlet var descriptionTextField: UITextField!
+    
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var cancelButton: UIButton!
+    
+    // MARK: - Variables
+    
+    var itemManager: ItemManager?
+    
+    lazy var geocoder = CLGeocoder()
+    lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
+    
+    // MARK: - ViewController lifecycle
+    
+    // MARK: - Actions
+    
+    func save() {
+        
+        guard let titleString = titleTextField.text, !titleString.isEmpty else {
+            return
+        }
+        
+        let date: Date?
+        if let dateText = self.dateTextField.text, !dateText.isEmpty {
+            date = dateFormatter.date(from: dateText)
+        } else {
+            date = nil
+        }
+        
+        let descriptionString = descriptionTextField.text
+        if let locationName = locationTextField.text, !locationName.isEmpty {
+            if let address = addressTextField.text, !address.isEmpty {
+                geocoder.geocodeAddressString(address) {
+                    [unowned self] (placeMarks, error) -> Void in
+                    
+                    let placeMarker = placeMarks?.first
+                    
+                    let item = ToDoItem(title: titleString, itemDescription: descriptionString, timestamp: date?.timeIntervalSince1970, location: Location(name: locationName, coordinate: placeMarker?.location?.coordinate))
+                    
+                    self.itemManager?.add(item)
+                    
+                }
+            }
+        }
+    }
+}
---
+++ ToDoTests/Controller/InputViewControllerTests.swift
@@ -0,0 +1,112 @@
+//
+//  InputViewControllerTests.swift
+//  ToDoTests
+//
+//  Created by Antonio da Silva on 21/07/2017.
+//  Copyright © 2017 TNTStudios. All rights reserved.
+//
+
+import XCTest
+import CoreLocation
+@testable import ToDo
+
+class InputViewControllerTests: XCTestCase {
+    
+    var sut: InputViewController!
+    var placeMark: MockPlaceMarker!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: "InputViewController") as! InputViewController
+        _ = sut.view
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test_HasTitleTextField() {
+        XCTAssertNotNil(sut.titleTextField)
+    }
+    
+    func test_HasDateTextField() {
+        XCTAssertNotNil(sut.dateTextField)
+    }
+    
+    func test_HasLocationTextField() {
+        XCTAssertNotNil(sut.locationTextField)
+    }
+    
+    func test_HasAddressTextField() {
+        XCTAssertNotNil(sut.addressTextField)
+    }
+    
+    func test_HasDrescriptionTextField() {
+        XCTAssertNotNil(sut.descriptionTextField)
+    }
+    
+    func test_HasSaveButton() {
+        XCTAssertNotNil(sut.saveButton)
+    }
+    
+    func test_HasCancelButton() {
+        XCTAssertNotNil(sut.cancelButton)
+    }
+    
+    func test_Save_UsesGeocoderToGetCoordinateFromAddress() {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        let timestamp = 1456070400.0 // 1456070400
+        let date = Date(timeIntervalSince1970: timestamp)
+        
+        sut.titleTextField.text = "Foo"
+        sut.dateTextField.text = dateFormatter.string(from: date)
+        sut.locationTextField.text = "Bar"
+        sut.addressTextField.text = "Infinite Loop 1, Cupertino"
+        sut.descriptionTextField.text = "Baz"
+        
+        let mockGeocoder = MockGeocoder()
+        sut.geocoder = mockGeocoder
+        
+        sut.itemManager = ItemManager()
+        
+        sut.save()
+        
+        placeMark = MockPlaceMarker()
+        let coordinate = CLLocationCoordinate2DMake(37.3316851, -122.0300674)
+        
+        placeMark.mockCoordinate = coordinate
+        mockGeocoder.completionHandler?([placeMark], nil)
+        
+        let item = sut.itemManager?.item(at: 0)
+        
+        let testItem = ToDoItem(title: "Foo", itemDescription: "Baz", timestamp: timestamp, location: Location(name: "Bar", coordinate: coordinate))
+        
+        XCTAssertEqual(item, testItem)
+        
+    }
+    
+}
+
+extension InputViewControllerTests {
+    class MockGeocoder: CLGeocoder {
+        var completionHandler: CLGeocodeCompletionHandler?
+        
+        override func geocodeAddressString(_ addressString: String, completionHandler: @escaping CLGeocodeCompletionHandler) {
+            self.completionHandler = completionHandler
+        }
+    }
+    class MockPlaceMarker: CLPlacemark {
+        var mockCoordinate: CLLocationCoordinate2D?
+        
+        override var location: CLLocation? {
+            guard let coordinate = mockCoordinate else {
+                return CLLocation()
+            }
+            return CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        }
+    }
+}
